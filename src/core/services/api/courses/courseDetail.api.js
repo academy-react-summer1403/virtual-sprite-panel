a3fc32e959b48af5cd885a147309e39572640d74
reza <rezakarimi74pnu@gmail.coma>
2024-11-18 23:46:53 +0330

    apidetailss

--- src/core/services/api/courses/courseDetail.api.js
+++ src/core/services/api/courses/courseDetail.api.js
@@ -2,7 +2,7 @@ import instance from "../../interceptor";
 
 export const getCoursesDetail = async (id) => {
   try {
-    const res = await instance.get(`/User/UserDetails/:UserId=${id}` );
+    const res = await instance.get(`/Course/${id}` );
     return res;
   } catch (error) {
     console.log( error); 
