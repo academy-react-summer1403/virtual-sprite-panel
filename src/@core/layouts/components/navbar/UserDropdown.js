f50614129842a6a7d5c5bdff7895254baa539715
Rezvaneh <rez.rm234@gmail.com>
2024-11-29 10:09:50 +0330

    admin

--- src/@core/layouts/components/navbar/UserDropdown.js
+++ src/@core/layouts/components/navbar/UserDropdown.js
@@ -37,7 +37,7 @@ const UserDropdown = () => {
         onClick={(e) => e.preventDefault()}
       >
         <div className="user-nav d-sm-flex d-none">
-          <span className="user-name fw-bold">John Doe</span>
+          {/* <span className="user-name fw-bold">John Doe</span> */}
           <span className="user-status">ادمین</span>
         </div>
         <Avatar
